ee717f300e4a435d17fab5710f250aaafdfcc46e
benrachedchourouk <119761287+benrachedchourouk@users.noreply.github.com>
2023-01-11 23:10:42 +0100

    ds terraform

---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "location" {
+  description = "the location of the vpc"
+  default     = "West Europe"
+}
+variable "username" {
+  description = "the location of the ressource"
+  default     = "adminuser"
+
+
+}
+
+variable "password" {
+  default = "chouroukbenrached123456789"
+}
